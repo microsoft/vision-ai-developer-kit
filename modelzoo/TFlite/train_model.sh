31e752d1f1f0bfe98db239f558e6dc10b8cda12e
fierval <fierval@gmail.com>
2019-02-28 15:14:06 -0800

    fix training script

--- modelzoo/TFlite/train_model.sh
+++ modelzoo/TFlite/train_model.sh
@@ -1,1 +1,9 @@
-python model_main.py --model_dir=/home/borisk/fpga/models/train --pipeline_config_path=/home/borisk/fpga/ssd_inception_v2_coco.config --num_train_steps=50000 --num_eval_steps=500 --logtostderr
+$CONFIG_FILE={path to the config file for the pipeline}
+$CHECKPOINT_OUTPUT_DIR={path to write checkpoint files to}
+
+python model_main.py \
+	--model_dir=$CHECKPOINT_OUTPUT_DIR \
+	--pipeline_config_path=$CONFIG_FILE \
+	--num_train_steps=50000 \ 
+	--num_eval_steps=500 \
+	--logtostderr
