78418cf8047c3e2aec80c3e51a77c8e324018856
David R. Williamson <drwill@microsoft.com>
2019-05-28 16:53:17 -0700

    Add copyright notice and readme.md

--- sample-solutions/VisionSampleToolset/IotEdgeSolutionNew/modules/WebStreamModule/scripts/mpeg4-stream.js
+++ sample-solutions/VisionSampleToolset/IotEdgeSolutionNew/modules/WebStreamModule/scripts/mpeg4-stream.js
@@ -1,3 +1,7 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for
+// full license information.
+
 const http = require('http');
 const ws = require('ws');
 const Process = require('child_process');
